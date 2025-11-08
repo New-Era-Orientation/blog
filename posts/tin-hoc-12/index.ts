b69ad685977079464ec55519dabe781c28831191
Nguyễn Duy Khánh <89499196+Desithedev@users.noreply.github.com>
2025-11-08 19:43:30 +0700

    fix import index.ts

--- posts/tin-hoc-12/index.ts
+++ posts/tin-hoc-12/index.ts
@@ -2,6 +2,7 @@
 import { post as vectorStlCpp } from './cpp002';
 import { post as oopPython } from './py003';
 import { post as mangMotChieu } from './tin12-001';
+import { post as chude12g } from './12g'
 
 export const tinHoc12Posts = [
   vectorStlCpp,
